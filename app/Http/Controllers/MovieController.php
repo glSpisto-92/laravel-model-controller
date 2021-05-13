de70b4c386e23e7a1cc925b58cfab62ec737d222
Gianluigi Spisto <luigi.spisto92@gmail.com>
2021-05-13 16:48:35 +0200

    Aggiunti film stampati a browser

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class MovieController extends Controller
 {
     public function index()
     {
-        return view('movies');
+        $movies = Movie::all();
+        return view('movies', ['movies' => $movies]);
     }
 }
