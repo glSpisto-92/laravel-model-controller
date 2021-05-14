47dbf8b0b7ba4d8814ca84ba38e33aeb5d609efc
Gianluigi Spisto <luigi.spisto92@gmail.com>
2021-05-14 16:05:31 +0200

    Aggiunto un pizzico di css

--- resources/views/movies/movies.blade.php
+++ resources/views/movies/movies.blade.php
@@ -4,12 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Db stamp</title>
+    {{-- google fonts --}}
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Goblin+One&display=swap" rel="stylesheet">
+    <title>Movies</title>
 </head>
 <body>
-    
    <div class="container"> 
         <div class="wrapper">
+            <h1>Movies List!</h1>
     @foreach ($movies as $movie)
             <ul>
                 <li><h2>{{$movie->titolo}}</h2></li>
@@ -23,20 +26,38 @@
     
     {{-- css al volo --}}
     <style>
+        body{
+            font-family: 'Goblin One', cursive;
+        }
         .container{
             /* display:flex; */
         }
         .wrapper{
-            width:calc(100% / 3);
             padding: 10px;
+        }
+        h1{
+            margin-bottom: 30px;
         }
         ul{
             list-style: none;
+            text-align: center;
+            margin-bottom: 50px;
+            background-color: lightgrey;
+            border-radius: 10px;
+            padding: 15px;
         }
          a{
             text-decoration: none;
-            display: block;
+            display: inline-block;
             margin-top: 20px;
+            background-color: coral;
+            border-radius: 10px;
+            padding: 10px;
+            color: #fff;
+        }
+        a:hover{
+            color: #000;
+            background-color: grey;
         }
     </style>
 </body>
--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -4,10 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Show</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Goblin+One&display=swap" rel="stylesheet">
+    <title>Movie description</title>
 </head>
 <body>
     <a href="{{route('movies.index')}}">Torna alla lista film</a>
     <p>{{$movie->descrizione}}</p>
 </body>
 </html>
+
+<style>
+    body{
+        font-family: 'Goblin One', cursive;
+    }
+    p{
+       max-width: 600px;
+       display: flex;
+       margin: 0 auto;
+       line-height: 2;
+       color: grey;
+    }
+    a{
+        text-decoration: none;
+        background-color: coral;
+            border-radius: 10px;
+            padding: 10px;
+            color: #fff;
+            margin-top: 10px;
+            display: inline-block;
+        }
+        a:hover{
+            color: #000;
+            background-color: grey;
+        }
+    }
+</style>
